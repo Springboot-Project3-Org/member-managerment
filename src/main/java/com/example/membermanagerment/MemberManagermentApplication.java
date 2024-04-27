8e7d405dda574f8a10c940404b9a121e81194a45
HaiCauTapCode <parkourdqk@gmail.com>
2024-04-27 20:49:48 +0700

    First Commit

---
+++ src/main/java/com/example/membermanagerment/MemberManagermentApplication.java
@@ -0,0 +1,13 @@
+package com.example.membermanagerment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManagermentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberManagermentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/membermanagerment/MemberManagermentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membermanagerment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberManagermentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
