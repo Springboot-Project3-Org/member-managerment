d96ad740abca4c66f5bb24e54dd0a0028dd3e836
ThangVo0308 <thangvo0308@gmail.com>
2024-05-03 10:10:47 +0700

    add/update/delete/search XuLy

--- src/main/java/com/example/membermanagerment/controller/AdminController.java
+++ src/main/java/com/example/membermanagerment/controller/AdminController.java
@@ -1,8 +1,10 @@
     package com.example.membermanagerment.controller;
     import com.example.membermanagerment.model.ThanhVien;
     import com.example.membermanagerment.model.ThietBi;
+    import com.example.membermanagerment.model.XuLy;
     import com.example.membermanagerment.repository.ThanhVienRepository;
     import com.example.membermanagerment.repository.ThietBiRepository;
+    import com.example.membermanagerment.repository.XuLyRepository;
     import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.stereotype.Controller;
@@ -13,9 +15,8 @@
     import org.springframework.web.bind.annotation.ResponseBody;
 
     import java.math.BigInteger;
-    import java.util.HashMap;
-    import java.util.List;
-    import java.util.Map;
+    import java.sql.Timestamp;
+    import java.util.*;
 
     @Controller
     public class AdminController {
@@ -24,6 +25,8 @@
 
         @Autowired
         private ThanhVienRepository thanhVienRepository;
+        @Autowired
+        private XuLyRepository xuLyRepository;
 
         @GetMapping("/admin")
         public String admin() {
@@ -203,7 +206,6 @@
         @ResponseBody
         public List<ThanhVien> searchMember(@RequestBody Map<String, String> searchData) {
             String searchValue = searchData.get("searchValue");
-            System.out.println("test thu value: "+searchValue);
             if (searchValue.isEmpty()) {
                 return thanhVienRepository.findAll();
             }
@@ -352,11 +354,158 @@
                 return searchResult;
             }
         // --------------------END DEVICE----------------------
-
-
-
+        // --------------------VI PHAM----------------------
         @GetMapping("/admin-vipham")
-        public String admin_vipham() {
+        public String admin_vipham(Model model) {
+            List<XuLy> errorList = xuLyRepository.findAll();
+            model.addAttribute("errorList",errorList);
             return "admin-vipham";
+        }
+
+        @PostMapping("/addError")
+        @ResponseBody
+        public Map<String, Object> addError(@RequestBody Map<String, String> errorData) {
+            Map<String, Object> response = new HashMap<>();
+
+            if (errorData.get("maTV") == null || errorData.get("maTV").isEmpty() ||
+                    errorData.get("hinhthuc") == null || errorData.get("hinhthuc").isEmpty() ||
+                    errorData.get("sotien") == null || errorData.get("sotien").isEmpty() ||
+                    errorData.get("ngayxuly") == null || errorData.get("ngayxuly").isEmpty()) {
+                response.put("success", false);
+                response.put("message", "Không được để trống các trường hoặc trạng thái không hợp lệ");
+                return response;
+            }
+
+            BigInteger maTV = new BigInteger(errorData.get("maTV"));
+            String hinhthuc = errorData.get("hinhthuc");
+            int sotien = Integer.parseInt(errorData.get("sotien"));
+            Timestamp ngayxuly = Timestamp.valueOf(errorData.get("ngayxuly"));
+            int status = Integer.parseInt(errorData.get("status"));
+
+            ThanhVien curMem = thanhVienRepository.findByMaTV(maTV);
+            if (curMem == null) {
+                response.put("success", false);
+                response.put("message", "Không tồn tại thành viên này");
+                return response;
+            }
+
+
+
+            XuLy newXuly = new XuLy(xuLyRepository.getMax(),maTV,hinhthuc,sotien,ngayxuly,status);
+            XuLy addedXuly = xuLyRepository.save(newXuly);
+
+            response.put("success", addedXuly != null);
+            if (addedXuly != null) {
+                response.put("message", "Thêm xử lý thành công");
+            } else {
+                response.put("message", "Thêm xử lý thất bại");
+            }
+            return response;
+        }
+
+        @PostMapping("/getError")
+        @ResponseBody
+        @JsonIgnoreProperties
+        public XuLy getErrorById(@RequestBody Map<String, Integer> requestData){
+            int maXL = requestData.get("maXL");
+            XuLy xuly = xuLyRepository.findById(maXL).orElse(null);
+            System.out.println("coi thu: "+xuly.toString());
+            return xuly;
+        }
+
+        @PostMapping("/editError")
+        @ResponseBody
+        public Map<String, Object> editError(@RequestBody Map<String, String> requestData) {
+            Map<String, Object> response = new HashMap<>();
+
+            if (requestData.get("maTV") == null || requestData.get("maTV").isEmpty() ||
+                    requestData.get("hinhthuc") == null || requestData.get("hinhthuc").isEmpty() ||
+                    requestData.get("sotien") == null || requestData.get("sotien").isEmpty() ||
+                    requestData.get("ngayxuly") == null || requestData.get("ngayxuly").isEmpty()) {
+                response.put("success", false);
+                response.put("message", "Không được để trống các trường hoặc trạng thái không hợp lệ");
+                return response;
+            }
+            if (requestData.get("maTV") == null || requestData.get("maTV").isEmpty()) {
+                response.put("success", false);
+                response.put("message", "Không được để trống các trường hoặc trạng thái không hợp lệ");
+                return response;
+            }
+
+            int maXL = Integer.parseInt(requestData.get("maXL"));
+            BigInteger maTV = new BigInteger(requestData.get("maTV"));
+            String hinhthuc = requestData.get("hinhthuc");
+            int sotien;
+            Timestamp ngayxuly = Timestamp.valueOf(requestData.get("ngayxuly"));
+            int status = Integer.parseInt(requestData.get("status"));
+
+            try{
+                sotien = Integer.parseInt(requestData.get("sotien"));
+            }catch(Exception e) {
+                response.put("success", false);
+                response.put("message", "Tiền không đúng định dạng");
+                return response;
+            }
+
+            ThanhVien curMem = thanhVienRepository.findByMaTV(maTV);
+            if (curMem == null) {
+                response.put("success", false);
+                response.put("message", "Không tồn tại thành viên này");
+                return response;
+            }
+
+            XuLy xuly = xuLyRepository.findById(maXL).orElse(null);
+            if (xuly.getTrangThaiXL() == 1) {
+                response.put("success", false);
+                response.put("message", "Trạng thái đã được xử lý, không thể điều chỉnh được nữa!!!");
+                return response;
+            }
+            XuLy curXuly = new XuLy(maXL,maTV,hinhthuc,sotien,ngayxuly,status);
+            XuLy updateXuly = xuLyRepository.save(curXuly);
+
+            response.put("success", updateXuly != null);
+            if (updateXuly != null) {
+                response.put("message", "Sửa xử lý thành công");
+            } else {
+                response.put("message", "Sửa xử lý thất bại");
+            }
+            return response;
+        }
+
+        @PostMapping("/deleteError")
+        @ResponseBody
+        @JsonIgnoreProperties
+        public Map<String, Object> deleteErrorById(@RequestBody Map<String, Integer> requestData) {
+            Map<String, Object> response = new HashMap<>();
+            int maXL = requestData.get("maXL");
+
+            XuLy xuly = xuLyRepository.findById(maXL).orElse(null);
+            if (xuly == null) {
+                response.put("success", false);
+                response.put("message", "Không tồn tại thiết bị này");
+                return response;
+            }
+
+            try {
+                xuLyRepository.delete(xuly);
+                response.put("success", true);
+                response.put("message", "Xóa thành công");
+            } catch (Exception e) {
+                response.put("success", false);
+                response.put("message", "Xóa thất bại");
+            }
+            return response;
+        }
+
+        @PostMapping("/searchError")
+        @ResponseBody
+        @JsonIgnoreProperties
+        public List<XuLy> searchError(@RequestBody Map<String, String> searchData) {
+            String searchValue = searchData.get("searchValue");
+            if(searchValue.isEmpty()) {
+                return xuLyRepository.findAll();
+            }
+            List<XuLy> searchResult = xuLyRepository.findByKeyword(searchValue);
+            return searchResult;
         }
     }
--- src/main/java/com/example/membermanagerment/model/XuLy.java
+++ src/main/java/com/example/membermanagerment/model/XuLy.java
@@ -2,11 +2,13 @@ package com.example.membermanagerment.model;
 
 import java.math.BigInteger;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "xuly")
@@ -19,6 +21,7 @@ public class XuLy {
     private int MaXL;
 
     @Column(name = "MaTV")
+    @JsonIgnore
     private BigInteger MaTV;
 
     @Column(name = "hinh_thucxl")
@@ -28,7 +31,7 @@ public class XuLy {
     private Integer SoTien;
 
     @Column(name = "NgayXL")
-    private Date NgayXL;
+    private Timestamp NgayXL;
 
     @Column(name = "trang_thaixl")
     private int TrangThaiXL;
@@ -65,11 +68,11 @@ public class XuLy {
         SoTien = soTien;
     }
 
-    public Date getNgayXL() {
+    public Timestamp getNgayXL() {
         return NgayXL;
     }
 
-    public void setNgayXL(Date ngayXL) {
+    public void setNgayXL(Timestamp ngayXL) {
         NgayXL = ngayXL;
     }
 
@@ -84,7 +87,7 @@ public class XuLy {
     public XuLy() {
     }
 
-    public XuLy(BigInteger thanhvien, String HinhThucXL, Integer SoTien, Date NgayXL, int TrangThaiXL) {
+    public XuLy(BigInteger thanhvien, String HinhThucXL, Integer SoTien, Timestamp NgayXL, int TrangThaiXL) {
         this.MaTV = thanhvien;
         this.HinhThucXL = HinhThucXL;
         this.SoTien = SoTien;
@@ -92,14 +95,36 @@ public class XuLy {
         this.TrangThaiXL = TrangThaiXL;
     }
 
-    public XuLy(BigInteger maTV, String hinhThucXL, Date ngayXL, int trangThaiXL) {
+    public XuLy(BigInteger maTV, String hinhThucXL, Timestamp ngayXL, int trangThaiXL) {
         this.MaTV = maTV;
         this.HinhThucXL = hinhThucXL;
         this.NgayXL = ngayXL;
         this.TrangThaiXL = trangThaiXL;
     }
 
+    public XuLy(int maXL, BigInteger maTV, String hinhThucXL, Integer soTien, Timestamp ngayXL, int trangThaiXL) {
+        MaXL = maXL;
+        MaTV = maTV;
+        HinhThucXL = hinhThucXL;
+        SoTien = soTien;
+        NgayXL = ngayXL;
+        TrangThaiXL = trangThaiXL;
+    }
+
+    @Override
+    public String toString() {
+        return "XuLy{" +
+                "MaXL=" + MaXL +
+                ", MaTV=" + MaTV +
+                ", HinhThucXL='" + HinhThucXL + '\'' +
+                ", SoTien=" + SoTien +
+                ", NgayXL=" + NgayXL +
+                ", TrangThaiXL=" + TrangThaiXL +
+                '}';
+    }
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MaTV", referencedColumnName = "MaTV", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_thanhvien_handle"))
+    @JsonIgnore
     private ThanhVien thanhvien;
 }
